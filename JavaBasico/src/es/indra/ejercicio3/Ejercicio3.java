afc686f0cc28d6039fefb1a8a0d24c02a6601a1c
Cursojava <Cursojava@192.168.1.16>
2019-01-14 18:41:52 +0100

    Subida de Sincronizacion

---
+++ JavaBasico/src/es/indra/ejercicio3/Ejercicio3.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package es.indra.ejercicio3;
+
+import java.util.Scanner;
+
+/**
+ * @author CURSOJAVA
+ *
+ */
+public class Ejercicio3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Introduzca un día de la semana:");
+		String dia = entrada.next
+	}
+
+}
