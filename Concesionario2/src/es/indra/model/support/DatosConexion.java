a6dac41389444f42a3d5246a01bd03491a851165
Cursojava <Cursojava@192.168.1.16>
2019-01-22 17:18:26 +0100

    Concesionario 2

--- Concesionario2/src/es/indra/model/support/DatosConexion.java
+++ Concesionario2/src/es/indra/model/support/DatosConexion.java
@@ -2,7 +2,7 @@ package es.indra.model.support;
 
 public class DatosConexion {
 
-	public static final String URL = "jdbc:postgresql://localhost/Concesionario";
+	public static final String URL = "jdbc:postgresql://localhost/Banco";
 	public static final String USUARIO_BD = "postgres";
 	public static final String PASSWORD_BD = "1234";
 
