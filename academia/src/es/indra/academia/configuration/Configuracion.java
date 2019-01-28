c9a5ada85cded2bf0ff4d9d34acaef3259f2daec
Cursojava <Cursojava@192.168.1.16>
2019-01-28 19:49:22 +0100

    Profesor incluido

--- academia/src/es/indra/academia/configuration/Configuracion.java
+++ academia/src/es/indra/academia/configuration/Configuracion.java
@@ -7,30 +7,31 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class Configuracion {
-	
+
 	private static Configuracion singleton = null;
 	private static String fichero = "configuracion.properties";
-	
+
 	Properties parametros;
-	
-	private Configuracion( ) {
+
+	private Configuracion() {
 		try {
 			this.parametros = new Properties();
-			InputStream propertiesStream = ClassLoader.getSystemResourceAsStream("fichero");
+			InputStream propertiesStream = getClass().getClassLoader().getResourceAsStream(fichero);
 			this.parametros.load(propertiesStream);
 			propertiesStream.close();
 		} catch (Exception e) {
-			
+
 		}
 	}
-	
+
 	public static Configuracion getInstance() {
 		if (singleton == null) {
 			singleton = new Configuracion();
 		}
 		return singleton;
+
 	}
-	
+
 	public Connection obtenerConexionBD() throws SQLException {
 		String url = this.parametros.getProperty("bbdd.url");
 		String usuario = this.parametros.getProperty("bbdd.usuario");
@@ -38,13 +39,15 @@ public class Configuracion {
 		try {
 			Class.forName("org.postgresql.Driver");
 		} catch (ClassNotFoundException e) {
+
 			e.printStackTrace();
 		}
 		Connection co = DriverManager.getConnection(url, usuario, pass);
 		return co;
 	}
-	
+
 	public String obtenerParametro(String parametro) {
 		return this.parametros.getProperty(parametro);
 	}
+
 }
---
+++ academia/src/es/indra/academia/model/entities/Profesor.java
@@ -0,0 +1,83 @@
+package es.indra.academia.model.entities;
+
+public class Profesor {
+	private Long id;
+	private String nombre;
+	private String apellido;
+	private String apellido2;
+	private String nif;
+	private String telefono;
+	private String correo;
+	private String titulacion;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getApellido() {
+		return apellido;
+	}
+	
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	public String getApellido2() {
+		return apellido2;
+	}
+	
+	public void setApellido2(String apellido2) {
+		this.apellido2 = apellido2;
+	}
+	
+	public String getNif() {
+		return nif;
+	}
+	
+	public void setNif(String nif) {
+		this.nif = nif;
+	}
+	
+	public String getTelefono() {
+		return telefono;
+	}
+	
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	
+	public String getCorreo() {
+		return correo;
+	}
+	
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	
+	public String getTitulacion() {
+		return titulacion;
+	}
+
+	public void setTitulacion(String titulacion) {
+		this.titulacion = titulacion;
+	}
+
+	@Override
+	public String toString() {
+		return "Profesor [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", apellido2=" + apellido2
+				+ ", nif=" + nif + ", telefono=" + telefono + ", correo=" + correo + ", titulacion=" + titulacion + "]";
+	}
+	
+}
