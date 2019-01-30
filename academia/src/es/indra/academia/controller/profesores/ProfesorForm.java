0959893a9b3ca971aa6bb85dc16de9b04ded7e0a
Cursojava <Cursojava@192.168.1.16>
2019-01-30 20:53:29 +0100

    Final academia

--- academia/src/es/indra/academia/controller/profesores/ProfesorForm.java
+++ academia/src/es/indra/academia/controller/profesores/ProfesorForm.java
@@ -2,6 +2,10 @@ package es.indra.academia.controller.profesores;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
+import es.indra.academia.controller.profesores.ProfesorForm;
+import es.indra.academia.model.entities.Alumno;
 import es.indra.academia.model.entities.Profesor;
 
 public class ProfesorForm extends Profesor {
@@ -15,6 +19,17 @@ public class ProfesorForm extends Profesor {
 		setTelefono("");
 		setCorreo("");
 		setTitulacion("");
+	}
+	
+	public ProfesorForm(Profesor a) {
+		super();
+		setId(a.getId());
+		setNif(a.getNif());
+		setNombre(a.getNombre());
+		setApellido1(a.getApellido1());
+		setApellido2(a.getApellido2());
+		setTelefono(a.getTelefono());
+		setCorreo(a.getCorreo());
 	}
 
 	public void validar(List<String> errores) {
@@ -35,5 +50,32 @@ public class ProfesorForm extends Profesor {
 
 		}
 
+	}
+	
+	public static ProfesorForm obtenerProfesorForm(HttpServletRequest request) {
+		String id = request.getParameter("id");
+		String nif = request.getParameter("nif");
+		String nombre = request.getParameter("nombre");
+		String apellido1 = request.getParameter("apellido1");
+		String apellido2 = request.getParameter("apellido2");
+		String telefono = request.getParameter("telefono");
+		String correo = request.getParameter("email");
+		
+		ProfesorForm profesor = new ProfesorForm();
+		Long idLong = null;
+		try {
+			idLong = Long.parseLong(id);
+		} catch (NumberFormatException e) {
+			idLong = null;
+		}
+
+		profesor.setId(idLong);
+		profesor.setCorreo(correo);
+		profesor.setApellido2(apellido2);
+		profesor.setApellido1(apellido1);
+		profesor.setNif(nif);
+		profesor.setNombre(nombre);
+		profesor.setTelefono(telefono);
+		return profesor;
 	}
 }
